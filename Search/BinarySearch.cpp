63cd25e8d1459879c0c35c796f08740f38a93f47
Madecat <k0shachiy@yandex.com>
2023-02-23 23:08:44 +0300

    Add recursive binary search

--- Search/BinarySearch.cpp
+++ Search/BinarySearch.cpp
@@ -1,7 +1,7 @@
 #include <iostream>	
 #include <vector>
 
-int binary_search(const std::vector<int> vector, int findable)
+int binary_search_non_recursive(const std::vector<int> vector, int findable)
 {
 	int begin = 0;
 	int end = vector.size() - 1;
@@ -28,9 +28,24 @@ int binary_search(const std::vector<int> vector, int findable)
 	return -1;
 }
 
+int binary_search_recursive(const std::vector<int> vector, int findable, int begin, int end)
+{
+	if (vector.at((begin + end) / 2) == findable)	{
+		return (begin + end) / 2;
+	}
+	
+	if (vector.at((end + begin) / 2) > findable)	{
+		return binary_search_recursive(vector, findable, begin, --end);
+	}
+	else {
+		return binary_search_recursive(vector, findable, --begin, end);
+	}
+}
+
 int main()
 {
 	const std::vector<int> vector = { 2, 8, 10, 23, 45, 67, 88, 89, 90, 100, 122, 144, 150, 888 };
-	std::cout << binary_search(vector, 67);
+	std::cout <<"Non-recursive: "<<binary_search_non_recursive(vector, 67);
+	std::cout<<"\nRecursive: "<<binary_search_recursive(vector, 88, 0, vector.size() - 1);
 	return EXIT_SUCCESS;
 }
